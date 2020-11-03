0d7ff0b0231bd8d116b10036e694036d79e97bb8
Mark <mdcaribe@mail.com>
2020-11-03 15:03:27 +0100

    jar loader

--- zaproxy/zap/src/main/java/org/zaproxy/zap/extension/policyverifier/Policy.java
+++ zaproxy/zap/src/main/java/org/zaproxy/zap/extension/policyverifier/Policy.java
@@ -3,7 +3,13 @@ package org.zaproxy.zap.extension.policyverifier;
 import java.util.Set;
 
 public class Policy {
-    private Set<Rule> policyRules;
+    private final Set<Rule> policyRules;
 
+    public Policy(Set<Rule> policyRules) {
+        this.policyRules = policyRules;
+    }
 
+    public Set<Rule> getPolicyRules() {
+        return policyRules;
+    }
 }
--- zaproxy/zap/src/main/java/org/zaproxy/zap/extension/policyverifier/PolicyVerifierModel.java
+++ zaproxy/zap/src/main/java/org/zaproxy/zap/extension/policyverifier/PolicyVerifierModel.java
@@ -1,27 +1,40 @@
 package org.zaproxy.zap.extension.policyverifier;
 
+import org.zaproxy.zap.extension.policyverifier.utils.JarLoader;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PolicyVerifierModel {
     private static PolicyVerifierModel soleModel;
-    private List<Policy> loadedPolicies = null;
+    private final List<Policy> loadedPolicies = new ArrayList<>();
 
-    private PolicyVerifierModel(){
-        if (soleModel != null){
+    private PolicyVerifierModel() {
+        if (soleModel != null) {
             throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
         }
     }
 
-    public static PolicyVerifierModel getSingleton(){
-        if (soleModel == null){
+    public static PolicyVerifierModel getSingleton() {
+        if (soleModel == null) {
             soleModel = new PolicyVerifierModel();
         }
         return soleModel;
     }
 
 
-    public void addPolicy(File file) {
-
+    public void addPolicy(File jar) {
+        JarLoader<Rule> loader = new JarLoader<>();
+        Set<Rule> rules = new HashSet<>();
+        try {
+            rules = loader.loadClassesInJar(jar, Rule.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        loadedPolicies.add(new Policy(rules));
     }
 }
